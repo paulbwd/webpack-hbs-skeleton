58f57b545d3352ca58ce7eab95996ba4c5f341f2
Paul Brown <paulbwd@gmail.com>
2017-06-11 15:33:17 +0100

    First Commit

---
+++ js/helpers/capitalize.js
@@ -0,0 +1,9 @@
+/**
+ * Created by PaulBrown on 11/06/2017.
+ */
+module.exports = function (text) {
+
+    var upperText = _.upperCase(text);
+    return (upperText);
+
+};
---
+++ js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by PaulBrown on 11/06/2017.
+ */
+var template = require('./hbs/hello.hbs');
+
+var output = document.getElementById("rendered_template");
+
+var data = {title: "Using Webpack and Handlebars Templating",
+            list_item:
+                [
+                    {colour: "red"},
+                    {colour: "green"},
+                    {colour: "blue"}
+                ], camel: _.camelCase("Camel case example using-lodash")
+    };
+    output.innerHTML = template(data);
